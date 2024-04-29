4354a48570024c8fcd4b03da8c403c5eaf040b13
MajorBriggs <158084320+danrcross@users.noreply.github.com>
2024-04-29 14:10:13 -0400

    changed queries to be more comprehensive in what they showed, in accordance with acceptance criteria!

--- modules/QueryMaker.js
+++ modules/QueryMaker.js
@@ -13,11 +13,17 @@ class QueryMaker {
   viewAll() {
     return `SELECT * FROM ${this.table}`;
   }
+  viewDept() {
+    return `SELECT r.id, r.title AS 'Role', d.name AS 'Department', r.salary AS 'Salary' FROM role r JOIN department d ON r.department_id=d.id`;
+  }
+  viewEmp() {
+    return `SELECT e1.id, CONCAT(e1.first_name,' ', e1.last_name) AS 'Employee', r.title AS 'Role', d.name AS 'Department', r.salary AS 'Salary', CONCAT(e2.first_name, ' ',e2.last_name) AS 'Manager' FROM employee e1 LEFT JOIN employee e2 ON e1.manager_id=e2.id JOIN role r ON e1.role_id=r.id JOIN department d ON r.department_id=d.id ORDER BY e1.id;`;
+  }
   viewEmpByM() {
-    return `SELECT * FROM employee WHERE manager_id=${this.managerId}`;
+    return `SELECT e1.id, CONCAT(e1.first_name,' ', e1.last_name) AS 'Employee', r.title AS 'Role', d.name AS 'Department', r.salary AS 'Salary', CONCAT(e2.first_name, ' ',e2.last_name) AS 'Manager' FROM employee e1 LEFT JOIN employee e2 ON e1.manager_id=e2.id JOIN role r ON e1.role_id=r.id JOIN department d ON r.department_id=d.id WHERE e1.manager_id=${this.managerId} ORDER BY e1.id;`;
   }
   viewEmpByD() {
-    return `SELECT e.* FROM department d JOIN role r ON d.id = r.department_id JOIN employee e ON r.id = e.role_id WHERE d.id=${this.deptId}`;
+    return `SELECT e1.id, CONCAT(e1.first_name,' ', e1.last_name) AS 'Employee', r.title AS 'Role', d.name AS 'Department', r.salary AS 'Salary', CONCAT(e2.first_name, ' ',e2.last_name) AS 'Manager' FROM employee e1 LEFT JOIN employee e2 ON e1.manager_id=e2.id JOIN role r ON e1.role_id=r.id JOIN department d ON r.department_id=d.id WHERE d.id=${this.deptId} ORDER BY e1.id`;
   }
   viewBudgetByD() {
     return `SELECT d.id AS 'Department ID', d.name AS 'Department', SUM(salary) AS 'Total Utilized Budget' FROM department d JOIN role r ON d.id = r.department_id JOIN employee e ON r.id = e.role_id WHERE d.id=${this.deptId}`;
